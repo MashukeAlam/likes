607e13e53f1a5e7b5f41d4ec025430e1fa145dfb
Mashuke Alam Jim <jimmashuke@gmail.com>
2024-11-08 10:33:00 +0600

    revert to erb.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,5 @@ module Likes
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.generators.template_engine = :slim
   end
 end
